bcbc305cdc6ba069f5898e1444329d3431d09155
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-13 01:05:20 +0800

    styling the images result with CSS grid

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -1,16 +1,13 @@
 import React from "react";
 
+import "./ImageList.css";
+
 const ImageList = ({ images }) => {
 	const image = images.map(({ urls, alt_description, id }) => (
-		<img
-			src={urls.regular}
-			alt={alt_description}
-			key={id}
-			className="ui image"
-		/>
+		<img src={urls.regular} alt={alt_description} key={id} />
 	));
 
-	return <div className="imagelist">{image}</div>;
+	return <div className="imageList">{image}</div>;
 };
 
 export default ImageList;
