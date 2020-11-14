cc7756779b25035437ffc5da1ab7318be94679e5
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-14 21:47:39 +0800

    applied ref to get the actual size of the image

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,11 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 export default class ImageCard extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { imageSpans: 0 };
+
+		this.imageRef = createRef();
+	}
+
+	componentDidMount() {
+		this.imageRef.current.addEventListener("load", this.setSpans);
+	}
+
+	setSpans = () => {
+		const imageHeight = this.imageRef.current.clientHeight;
+		const imageSpans = Math.ceil(imageHeight / 10 + 1);
+
+		this.setState({ imageSpans });
+	};
+
 	render() {
 		const { urls, description } = this.props.image;
 		return (
-			<div>
-				<img src={urls.regular} alt={description} />
+			<div style={{ gridRowEnd: `span ${this.state.imageSpans}` }}>
+				<img ref={this.imageRef} src={urls.regular} alt={description} />
 			</div>
 		);
 	}
